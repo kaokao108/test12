d00d26484639043209b1c317fb7243c2b60b7d86
kaokao <st200530415@gmail.com>
2017-11-16 20:19:02 +0800

    zz

--- index.js
+++ index.js
@@ -156,13 +156,13 @@ var jp = function(){
       // var target3 = $(".PrintShowTimesSession")
       // console.log(target[14].children[0].data);
       // var showtimes = []
-      // var movie = target[0].children[0].data;
+      var movie = target.children.data;
       // var movie2 = target2[0].children[0].data;
       // var movie3 = target3[0].children[0].data;
       // for(var i=0 ; i<titles.length ; i++) {
       //   result.push($(titles[i]).text());
       bot.on('message',function(event){
-          event.reply(target);
+          event.reply(movie);
         });
       // if (jp > 0) {
        //  bot.on('message',function(event){
